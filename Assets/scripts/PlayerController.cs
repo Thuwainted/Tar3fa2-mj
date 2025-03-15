859f509b81648fadb48d02553caf7cf39dc762ce
Thuwainted <a55105@alunos.ipb.pt>
2025-03-15 18:05:24 +0000

    Create Input Actions for player character movement
    Scripts change and player propertys

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour 
 {
+
     // Called once when the object is initialized
+    public InputAction MoveAction;
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Called once per frame
     void Update()
     {
+      
         float horizontal = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed)
         {
@@ -37,11 +39,11 @@ public class PlayerController : MonoBehaviour
         {
             vertical = -1.0f;
         }
-
         Debug.Log(vertical);
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        position.y = position.y + 0.1f * vertical;
+
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
 
     }
